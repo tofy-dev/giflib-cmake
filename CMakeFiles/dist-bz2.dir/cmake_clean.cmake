820ea6bba31c07f4cd7b356029b9abdabd7d6173
tofy-dev <tofy@disroot.org>
2023-03-11 01:28:45 -0800

    delete files

--- CMakeFiles/dist-bz2.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/dist-bz2"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/dist-bz2.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
